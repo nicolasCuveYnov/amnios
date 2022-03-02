a01fa7ecfed1dcd1f927533cba12615909b05f74
Nicolas <nicolas.cuve@ynov.com>
2022-03-02 11:39:34 +0100

    add auth sys

--- amnios/api/views.py
+++ amnios/api/views.py
@@ -2,33 +2,39 @@ from rest_framework import viewsets
 
 from .models import Agency, Seller, Buyer, EstateAgent, Estate, VisitSlot
 from .serializers import AgencySerializer, SellerSerializer, BuyerSerializer, EstateAgentSerializer, EstateSerializer, VisitSlotSerializer
-
+from rest_framework.permissions import IsAuthenticated, DjangoModelPermissions
 
 class AgencyViewSet(viewsets.ModelViewSet):
+    permission_classes = (DjangoModelPermissions,)
     serializer_class = AgencySerializer
     queryset = Agency.objects.all()
 
 
 class SellerViewSet(viewsets.ModelViewSet):
+    permission_classes = (DjangoModelPermissions,)
     serializer_class = SellerSerializer
     queryset = Seller.objects.all()
 
 
 class BuyerViewSet(viewsets.ModelViewSet):
+    permission_classes = (DjangoModelPermissions,)
     serializer_class = BuyerSerializer
     queryset = Buyer.objects.all()
 
 
 class EstateAgentViewSet(viewsets.ModelViewSet):
+    permission_classes = (DjangoModelPermissions,)
     serializer_class = EstateAgentSerializer
     queryset = EstateAgent.objects.all()
 
 
 class EstateViewSet(viewsets.ModelViewSet):
+    permission_classes = (DjangoModelPermissions,)
     serializer_class = EstateSerializer
     queryset = Estate.objects.all()
 
 
 class VisitSlotViewSet(viewsets.ModelViewSet):
+    permission_classes = (DjangoModelPermissions,)
     serializer_class = VisitSlotSerializer
     queryset = VisitSlot.objects.all()
