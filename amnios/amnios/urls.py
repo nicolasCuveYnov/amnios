70dbfe3426dab9ae074a8325872d050c6ed181fe
Nicolas <nicolas.cuve@ynov.com>
2022-04-13 11:12:23 +0200

    add openapi route

--- amnios/amnios/urls.py
+++ amnios/amnios/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from rest_framework.schemas import get_schema_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('openapi/', get_schema_view(title="API ESTATE",description="API developers hpoing to use our service"), name='openapi-schema')
 ]
